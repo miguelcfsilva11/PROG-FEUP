636b3b08f774452a3d941f60febb0c3fe2d192ec
Miguel Silva <72610584+miguelcfsilva11@users.noreply.github.com>
2021-03-13 22:22:22 +0000

    Solved 16th Exercise

---
+++ Ficha 2/Exercise_2.16.cpp
@@ -0,0 +1,57 @@
+// Program that simulates a calculator and stores values into memory allowing the user to manipulate them using the given commands
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	double op1, op2, result, M;
+	string type, choice;
+	cout << "This program executes simple algebraic operations" << endl;
+	cout << "Possible operations (other than addition and subtraciton):\n* - for multiplying\n/ - for dividing\n" << endl;
+	while (true) {
+		cout << "First operand: ";
+		cin >> op1;
+		cout << "Second operand: ";
+		cin >> op2;
+		cout << "Type of operation: ";
+		cin >> type;
+		if (type == "*") {
+			cout << op1 * op2 << endl;
+			result = op1 * op2;
+		}
+		else if (type == "-") {
+			cout << op1 - op2 << endl;
+			result = op1 - op2;
+		}
+		else if (type == "+") {
+			cout << op1 + op2 << endl;
+			result = op1 + op2;
+		}
+		else if (type == "/") {
+			cout << op1 / op2 << endl;
+			result = op1 / op2;
+		}
+		cout << "What do you want to do with this result?\nPress M to save it, MC to to clear the memory\n";
+		cout << "C to clear the contents of the display\nM+ or M- to add or subtract its content to the memory\n";
+		cout << "And finaly, MR to show the content saved in memory.\n Pick one of these options (write STOP to leave): ";
+		cin >> M;
+		if (choice == "M")
+			M = result;
+		else if (choice == "MR")
+			cout << M << endl;
+		else if (choice == "C")
+			cout << string(50, '\n');
+		else if (choice == "M-")
+			M -= result;
+		else if (choice == "M+")
+			M += result;
+		else if (choice == "MC")
+			M = 0;
+		else if (choice == "STOP")
+			break;
+	}
+	return 0;
+}
