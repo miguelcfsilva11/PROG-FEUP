c7ddea50d3edca9dd23b34e75541e7c340687f7b
Miguel Silva <72610584+miguelcfsilva11@users.noreply.github.com>
2021-03-13 15:10:44 +0000

    Solved Part C

--- Ficha 2/Exercise_2.13.cpp
+++ Ficha 2/Exercise_2.13.cpp
@@ -1,4 +1,5 @@
 // B) - Program that computes the largest, the smallest, the sum and the mean out of all the numbers of a sequence
+// To end the sequence, press 0
 
 #include <iostream>
 #include <cmath>
@@ -30,6 +31,40 @@ int main()
 	cout << "Min: " << min << endl;
 
 	return 0;
-
-
 }
+
+// C) - To end the sequence, press CTRL+Z
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+	double x, sum = 0, counter = 0;
+	int max = INT_MIN;
+	int min = INT_MAX;
+	do {
+		cout << "Insert a number: ";
+		cin >> x;
+		if (not cin.eof()) {
+			sum = sum + x;
+			counter++;
+			if (x > max)
+				max = x;
+			if (x < min)
+				min = x;
+		}
+	} while (not cin.eof());
+
+	cout << "Mean: " << sum / counter << endl;
+	cout << "Sum :" << sum << endl;
+	cout << "Max: " << max << endl;
+	cout << "Min: " << min << endl;
+
+	return 0;
+
+
+} 
