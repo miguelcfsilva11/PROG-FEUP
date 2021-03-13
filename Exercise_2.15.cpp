4b1ccbde0c2a79c948afb78108dfc67f2214afac
Miguel Silva <72610584+miguelcfsilva11@users.noreply.github.com>
2021-03-13 16:25:04 +0000

    Solved 15th Exercise

---
+++ Exercise_2.15.cpp
@@ -0,0 +1,39 @@
+// Program that checks if the user knows how to multiply the given numbers. If he takes more than 10 seconds, that answer will be graded as "Insufficient!"
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <chrono>
+using namespace std;
+using namespace chrono;
+
+
+int main()
+{
+	steady_clock sc;
+	auto start = sc.now();
+	int answer;
+	int num1 = rand() % 8 + 2;
+	int num2 = rand() % 8 + 2;
+	int result = num1 * num2; 
+	cout << "What's the result of " << num1 << " x " << num2 << "? ";
+	cin >> answer;
+	auto end = sc.now();
+	auto time_span = static_cast<duration<double>>(end - start);
+	if (answer != result) {
+		cout << "Very Bad!" << endl;
+		return 0;
+	}
+	else if (time_span.count() < 5) {
+		cout << "Good!" << endl;
+		return 0;
+	}
+	else if (5 <= time_span.count() && time_span.count() < 10) {
+		cout << "Satisfactory!" << endl;
+		return 0;
+	}
+	else if (time_span.count() > 10) {
+		cout << "Insufficient!" << endl;
+		return 0;
+	}
+}
