9e426af715eaf440329458840406aa930980bde5
Miguel Silva <72610584+miguelcfsilva11@users.noreply.github.com>
2021-03-24 17:27:43 +0000

    Solved 6th Exercise

--- Ficha 3/Exercise_3.6.cpp
+++ Ficha 3/Exercise_3.6.cpp
@@ -1,1 +1,30 @@
+// Program that calculates the time elapsed between two pressed keys
 
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+time_t timeElapsed() {
+
+	return time(NULL);
+}
+
+int main() {
+	double start, end;
+	char pass;
+
+	cout << "Press any key to start the clock: ";
+	cin >> pass;
+	start = timeElapsed();
+	cout << "The clock has started" << endl;
+	cout << "Press any key to stop the clock: ";
+	cin >> pass;
+	end = timeElapsed();
+	cout << "The clock has stopped" << endl;
+	cout << "Total amount of time: " << (end - start) << endl;
+
+	return 0;
+}
