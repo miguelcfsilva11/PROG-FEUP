0ecd191528e09cdcb1ad99fce9e4593d1f3293f1
Miguel Silva <72610584+miguelcfsilva11@users.noreply.github.com>
2021-03-25 11:43:07 +0000

    Solved Part B

--- Ficha 3/Exercise_3.9.cpp
+++ Ficha 3/Exercise_3.9.cpp
@@ -1,4 +1,4 @@
-// Program that verifies whether a given year is a leap year
+// A) - Program that verifies whether a given year is a leap year
 
 #include <iostream>
 #include <cmath>
@@ -31,5 +31,50 @@ int main() {
 		cout << "It is not a leap year!" << endl;
 	}
 	
+	return 0;
+}
+
+// B) - Program that checks the numbers days a month has in a given year
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+bool isLeap(int year)
+{
+	if (year % 400 == 0)
+		return true;
+	else if(year % 4 == 0 && year % 100 != 0)
+		return true;
+	else
+		return false;
+}
+
+int monthDays(int& month, int& year)
+{
+	char separator;
+	cout << "Pick a month and a year (separated by '/'): ";
+	cin >> month >> separator >> year;
+	switch (month){
+	case 1: case 3: case 5: case 7: case 8: case 10: case 12: return 31;
+		case 2:
+			if (isLeap(year))
+				return 29;
+			else
+				return 28;
+	case 4: case 6: case 9: case 11: return 30;
+	}
+
+}
+
+
+int main() {
+
+	int days, month, year;
+	days = monthDays(month, year);
+	cout << "That month has " << days << " days!" << endl;
 	return 0;
 }
