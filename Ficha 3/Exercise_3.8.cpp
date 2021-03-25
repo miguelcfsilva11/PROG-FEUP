75a26ddb54f2a5dbc6ef77d9238c62a2c9d9c52c
Miguel Silva <72610584+miguelcfsilva11@users.noreply.github.com>
2021-03-25 10:38:34 +0000

    Solved Part A

--- Ficha 3/Exercise_3.8.cpp
+++ Ficha 3/Exercise_3.8.cpp
@@ -1,1 +1,31 @@
+// A) - Program that reads a fraction
 
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+bool readFraction(int& numerator, int& denominator)
+{
+	char op;
+	cout << "Insert a fraction (separated by '/'): ";
+	cin >> numerator >> op >> denominator;
+	if (cin.fail() || cin.peek() != '\n' || op != '/')
+		return false;
+	else
+		return true;
+
+}
+
+int main() {
+
+	int n, d;
+	if (readFraction(n, d))
+		cout << "It's valid!" << endl;
+	else
+		cout << "Wrong Input!" << endl;
+
+	return 0;
+}
