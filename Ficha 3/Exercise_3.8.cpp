a82735e1199498b91295fbaf23d386a0f3d404f4
Miguel Silva <72610584+miguelcfsilva11@users.noreply.github.com>
2021-03-25 10:45:34 +0000

    Solved Part B

--- Ficha 3/Exercise_3.8.cpp
+++ Ficha 3/Exercise_3.8.cpp
@@ -29,3 +29,43 @@ int main() {
 
 	return 0;
 }
+
+// B) - Program that reduces a fraction by calculating the greatest common divisor between both numbers
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+int gdc(int n1, int n2) // recursive version
+{
+	if (n1 % n2 == 0)
+		return n2;
+	else
+		return gdc(n2, n1 % n2);
+}
+
+void reduceFraction(int& numerator, int& denominator)
+{
+	int common_divisor;
+	char op;
+	cout << "Insert a fraction (separated by '/'): ";
+	cin >> numerator >> op >> denominator;
+	if (cin.fail() || cin.peek() != '\n' || op != '/')
+		cout << "Wrong operation!" << endl;
+	else
+	{
+		common_divisor = gdc(numerator, denominator);
+		cout << "Its reduced form is: " << numerator / common_divisor << "/" << denominator / common_divisor << endl;
+	}
+
+}
+
+int main() {
+
+	int n, d;
+	reduceFraction(n, d);
+	return 0;
+}
