1983fb109387362a9a40d54f5c7783e7d0363bc9
Miguel Silva <miguelcfsilva@gmail.com>
2021-04-05 19:58:32 +0100

    Solved Part C

--- Ficha 4/Exercise_4.1.cpp
+++ Ficha 4/Exercise_4.1.cpp
@@ -7,8 +7,11 @@
 
 using namespace std;
 
-int findValueInArray(const int a[], size_t nElem, int value, size_t pos1, size_t pos2)
+int findValueInArray(const int a[], size_t nElem, int value, size_t pos1 = 0, size_t pos2 = 10000)
 {
+    if (pos2 == 10000)
+        pos2 = sizeof(a);
+
     for(size_t i = pos1; i < pos2; i++)
     {
         if (a[i] == value)
@@ -30,26 +33,37 @@ void readArray(int a[], size_t nElem)
 int main()
 
 {
-    int a[10000], value;
-    char comma;
+    int a[10000], value, result;
+    char comma, answer;
     size_t size_array, pos1, pos2;
     cout << "What would be the size of your array? ";
     cin >> size_array;
     readArray(a, size_array);
     cout << "Which value would you want to search: ";
     cin >> value;
-    cout << "Index limits of search (separated by commas): ";
-    cin >> pos1 >> comma >> pos2;
-    int result = findValueInArray(a, size_array, value, pos1, pos2);
-    if (result == -1)
-    {
-        cout << "Value not found!" << endl;
+    cout << "Do you want to establish some limits? (Y/N): ";
+    cin >> answer;
+    if (answer == 'Y' | answer == 'y'){
+        cout << "Index limits of search (separated by commas): ";
+        cin >> pos1 >> comma >> pos2;
+        result = findValueInArray(a, size_array, value, pos1, pos2);
     }
     else
     {
-        cout << "Value found at index " << result << "." << endl;
+        result = findValueInArray(a, size_array, value);
     }
 
+    if (result == -1)
+
+    {
+        cout << "Value not found!" << endl;
+    }
+
+    else
+
+    {
+        cout << "Value found at index " << result << "." << endl;
+    }
 
     return 0;
 
