11bd308f8e19f6401a724f4e59cbb1d7cb236a8b
Miguel Silva <miguelcfsilva@gmail.com>
2021-04-05 19:36:38 +0100

    Solved Part B

--- Ficha 4/Exercise_4.1.cpp
+++ Ficha 4/Exercise_4.1.cpp
@@ -7,6 +7,17 @@
 
 using namespace std;
 
+int findValueInArray(const int a[], size_t nElem, int value, size_t pos1, size_t pos2)
+{
+    for(size_t i = pos1; i < pos2; i++)
+    {
+        if (a[i] == value)
+            return i;
+    }
+
+    return -1;
+}
+
 void readArray(int a[], size_t nElem)
 {
     for(size_t i = 0; i < nElem; i++)
@@ -19,11 +30,27 @@ void readArray(int a[], size_t nElem)
 int main()
 
 {
-    int a[10000];
-    size_t size_array;
+    int a[10000], value;
+    char comma;
+    size_t size_array, pos1, pos2;
     cout << "What would be the size of your array? ";
     cin >> size_array;
     readArray(a, size_array);
+    cout << "Which value would you want to search: ";
+    cin >> value;
+    cout << "Index limits of search (separated by commas): ";
+    cin >> pos1 >> comma >> pos2;
+    int result = findValueInArray(a, size_array, value, pos1, pos2);
+    if (result == -1)
+    {
+        cout << "Value not found!" << endl;
+    }
+    else
+    {
+        cout << "Value found at index " << result << "." << endl;
+    }
+
+
     return 0;
 
 }
