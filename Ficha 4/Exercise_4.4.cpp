e10128145a27e4bf07ef4cc7bdd5ff29d9137fa8
miguelcfsilva11 <miguelcfsilva@gmail.com>
2021-04-18 22:15:40 +0100

    Solved 4th Exercise

--- Ficha 4/Exercise_4.4.cpp
+++ Ficha 4/Exercise_4.4.cpp
@@ -1,15 +1,53 @@
-// Program that implements bubble sort algorithm
+// Program that implements binary search algorithm
 
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
-int main()
+int binarySearch(const vector<int> &v, int value){
 
+    int first = 0;
+    int last = v.size() - 1;
+    int middle;
+    
+    while (first <= last){
+        middle = (first + last)/2;
+        if (v[middle] == value)
+            return middle;
+        else if (value < v[middle])
+            last = middle - 1;
+        else
+            first = middle + 1;
+
+    }
+    return -1;
+
+}
+
+int main()
 {
+    vector<int> v;
+    int value;
+    cout << "Fill your array with numbers (separated by spaces): ";
+     do
+     {
+        cin >> value;
+        v.push_back(value);
+
+     } while (cin.peek() != '\n');
+
+    cout << "Choose the value that we must search for: ";
+    cin >> value;
+
+    int search = binarySearch(v, value);
+    if (search == -1)
+        cout << "We did not find the value!" << endl;
+    else
+        cout << "The value is at the index number " << search << "." << endl;
 
     return 0;
 
