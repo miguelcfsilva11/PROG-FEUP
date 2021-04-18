a9a8a297f3f3a7eafd5e01f354038493610de142
miguelcfsilva11 <miguelcfsilva@gmail.com>
2021-04-18 16:39:26 +0100

    Updating Description

---
+++ Ficha 4/Exercise_4.2.cpp
@@ -0,0 +1,16 @@
+// Program that refactors the previous exercise
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+int main()
+
+{
+
+    return 0;
+
+}
