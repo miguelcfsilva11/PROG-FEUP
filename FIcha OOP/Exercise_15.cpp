141bf6e70edd851f30c91ac5208417686d0b1eb2
miguelcfsilva11 <miguelcfsilva@gmail.com>
2021-06-16 22:32:57 +0100

    Implemented Operator Overload

---
+++ FIcha OOP/Exercise_15.cpp
@@ -0,0 +1,93 @@
+// Program that works with the main aspects of OOP
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+class Date
+{
+
+friend bool operator== (Date date1, Date date2);
+friend ostream& operator<< (ostream &out, Date date1);
+
+public:
+
+    Date();
+    Date(unsigned int year, unsigned int month,unsigned int day);
+    Date(string yearMonthDay); // yearMonthDay must be in format "yyyy/mm/dd"
+    void setYear(unsigned int year);
+    void setMonth(unsigned int month);
+    void setDay(unsigned int day);
+    void setDate(unsigned int year, unsigned int month, unsigned int day);
+    unsigned int getYear() const;
+    unsigned int getMonth() const;
+    unsigned int getDay() const;
+    string getDate() const; // returns the date in format "yyyy/mm/dd"
+    void show() const; // shows the date on the screen in format "yyyy/mm/dd"
+
+private:
+
+    unsigned int year;
+    unsigned int month;
+    unsigned int day;
+
+};
+
+bool operator== (Date date1, Date date2)
+{
+    if (date1.getYear() == date2.getYear() &&
+        date1.getMonth() == date2.getMonth() &&
+        date1.getDay() == date2.getDay())
+    {
+        return true;
+    }
+    return false;
+
+}
+
+ostream& operator<< (ostream &out, Date date1)
+{
+    out << "Year: " << date1.getYear() << "\n";
+    out << "Month: " << date1.getMonth() << "\n";
+    out << "Day: " << date1.getDay() << endl;
+    return out;
+}
+
+Date::Date(){
+    this->year = 2002;
+    this->month = 11;
+    this->day = 22;
+
+}
+
+Date::Date(unsigned int year, unsigned int month,unsigned int day){
+    this->year = year;
+    this->month = month;
+    this->day = day;
+}
+
+void Date::setDate(unsigned int year, unsigned int month,unsigned int day){
+    this->year = year;
+    this->month = month;
+    this->day = day;
+}
+
+void Date::setYear(unsigned int year){
+    this->year = year;
+}
+
+unsigned int Date::getYear() const {
+    cout << this->year << endl;
+    return 0;
+}
+
+
+
+int main()
+{
+    Date birthday(2002,9,29);
+    birthday.getYear();
+}
